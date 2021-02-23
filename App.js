7b13742d7c94c5d2e61eb935d0fbe8aca4a32855
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2021-02-23 00:53:51 +0000

    removed some screens to tab nav

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 
-import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator, HeaderBackButton} from '@react-navigation/stack';
 
 
 
 
-import { NavigationContainer } from '@react-navigation/native';
+
+import { NavigationContainer, useBackButton } from '@react-navigation/native';
 
 
 import signup from './components/signup';
 import login from './components/SignIn';
 import home from './components/home';
-import getUser from './components/getUserDetails';
+
 //import signup from '../components/signup';
-import Update from './components/updateUser';
-import LocatinInfo from './components/locationInfo';
-import add_review from './components/addReview';
-import ViewReviews from './components/viewMyReviews';
-import update_review from './components/updateReview';
-import SearchUser from './components/search';
-import ViewLikedReviews from './components/viewLikedReviews';
+
+import LocatinInfo from './components/shared/locationInfo';
+import add_review from './components/shared/addReview';
+import ViewReviews from './components/shared/viewMyReviews';
+import update_review from './components/shared/updateReview';
+//import SearchUser from './components/search';
+//<Stack.Screen name= "SearchUser" component = {SearchUser}></Stack.Screen>
+import ViewLikedReviews from './components/shared/viewLikedReviews';
 const Stack = createStackNavigator();
 
 class App extends React.Component {
@@ -34,14 +36,21 @@ render()
         <Stack.Navigator>
         <Stack.Screen name= "login" component = {login}></Stack.Screen>
         <Stack.Screen name= "signup" component = {signup}></Stack.Screen>
-        <Stack.Screen name= "home" component = {home}></Stack.Screen>
-        <Stack.Screen name= "Update" component = {Update}></Stack.Screen>
-        <Stack.Screen name= "getUser" component = {getUser}></Stack.Screen>
+        <Stack.Screen name= "home" component = {home} ></Stack.Screen>
+        
+        
         <Stack.Screen name= "LocatinInfo" component = {LocatinInfo}></Stack.Screen>
         <Stack.Screen name= "add_review" component = {add_review}></Stack.Screen>
         <Stack.Screen name= "ViewReviews" component = {ViewReviews}></Stack.Screen>
         <Stack.Screen name= "update_review" component = {update_review}></Stack.Screen>
-        <Stack.Screen name= "SearchUser" component = {SearchUser}></Stack.Screen>
+
+
+        
+        
+
+
+
+
         <Stack.Screen name= "ViewLikedReviews" component = {ViewLikedReviews}></Stack.Screen>
         
         
