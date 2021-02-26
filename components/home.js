91f6d681042d4cda9475122601669731f5c954c5
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2021-02-26 13:56:05 +0000

    implemented pagination in home screen

--- components/home.js
+++ components/home.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
-//import React, { Component } from 'react';
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+//import React, { PureComponent } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Rating, AirbnbRating } from 'react-native-ratings';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,6 +9,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 //import Ionicons from 'react-native-vector-icons/Ionicons';
 import styles from '../Styling/stylingSheet';
+import { color } from 'react-native-reanimated';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {
  
@@ -28,10 +29,10 @@ import {
 } from 'react-native';
 
 
-import { color } from 'react-native-reanimated';
 
 
-class Home extends PureComponent {
+
+class Home extends React.Component {
   constructor(props) {
 
     super(props);
@@ -43,18 +44,21 @@ class Home extends PureComponent {
       isLoading: true,
     
       locations: null,
+
+      pageCount: '',
+      pageNum: 0,
+      initialNum: 100
+
+
       
     };
 
   }
 
-
-
-    displayCoffeeShops = async () => {
-      console.log('we are in user details');
-    let id = await  AsyncStorage.getItem('@user_id');
+  retrieveAllLocations = async () => {
+    console.log("retrieve function keeps updating");
     let token = await  AsyncStorage.getItem('@session_token');
-    return fetch('http://10.0.2.2:3333/api/1.0.0/find', {
+    return fetch('http://10.0.2.2:3333/api/1.0.0/find?limit='+this.state.initialNum, {
         method: 'get',
         headers: {
           'Content-Type': 'application/json',
@@ -63,6 +67,48 @@ class Home extends PureComponent {
       })
       .then((response) => {
         if(response.status === 200) {
+         
+          return response.json()
+        }else{
+          throw 'Somthing went wrong';
+        }
+      })
+      .then(async (responseJson) => {
+        this.setState ({
+          pageCount: responseJson.length,
+          
+        });
+       
+        console.log(this.state.pageCount , "PAGE COUNT");
+
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    
+    }
+
+
+
+    displayCoffeeShops = async (page) => {
+      console.log("display function keeps updating");
+      this.setState ({
+        pageNum : page
+      })
+    let pageOffset = page * 5;
+    let id = await  AsyncStorage.getItem('@user_id');
+    let token = await  AsyncStorage.getItem('@session_token');
+    return fetch('http://10.0.2.2:3333/api/1.0.0/find?offset='+pageOffset + '&limit=' + 5, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-authorization' : token
+        }
+      })
+      .then((response) => {
+        if(response.status === 200) {
+          console.log(this.state.pageNum , " page")
+         
           return response.json()
         }else{
           throw 'Somthing went wrong';
@@ -133,7 +179,8 @@ class Home extends PureComponent {
 
   componentDidMount(){
     this.unsubscribe = this.props.navigation.addListener('focus', ()=>{
-      this.displayCoffeeShops();
+      this.retrieveAllLocations();
+      this.displayCoffeeShops(this.state.pageNum);
     });
   }
   componentWillUnmount(){
@@ -147,8 +194,29 @@ class Home extends PureComponent {
  
 
     onRefresh = () => {
-      this.displayCoffeeShops();
+      this.retrieveAllLocations();
+      this.displayCoffeeShops(this.state.pageNum);
       console.log("redsfsfres")
+    }
+
+    
+    nextPage = (page) => {
+      if (this.state.pageNum * 5 <this.state.pageCount-5){
+        this.displayCoffeeShops(page)
+        this.setState({pageNum : page})
+        console.log(this.state.pageNum);
+      }
+
+    }
+
+      // onPress={() => {this.state.pageNumber===0?'':this.getAllLocationsPaged(this.state.pageNumber-1)}}>
+    perviousPage = (page) => {
+      if (this.state.pageNum > 0){
+        this.displayCoffeeShops(page)
+        this.setState({pageNum : page})
+        console.log(this.state.pageNum);
+      }
+
     }
 
     
@@ -174,16 +242,44 @@ class Home extends PureComponent {
       
 
       
-    <View style={{flex:1 }}> 
+    <View style= {styles.container}> 
 
         <View style={styles.fixToText}> 
 
+              <TouchableOpacity
+                      style={styles.formTouch}
+                      onPress={() => {this.perviousPage(this.state.pageNum -1)}}>
+                      
+                        {this.state.pageNum===0?<Text >''</Text>:<Ionicons name= {"chevron-back-sharp"} size = {23}/>}
+              
+                    </TouchableOpacity>
+               
+        
               <TouchableOpacity
                 style={styles.buttonStyle}
                 onPress={() => this.signOutAlert()}
                >
                 <Text style={styles.formTouchText}>logout <Ionicons name= {"log-out-outline"} size = {25}/></Text>
               </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.formTouch}
+                onPress={() => {this.nextPage(this.state.pageNum+1)}}>
+                 
+                {this.state.pageNum * 5 <this.state.pageCount-5?<Ionicons name= {"chevron-forward-sharp"} size = {23}/>:<Text>''</Text>}
+
+              </TouchableOpacity>
+
+
+
+           
+
+                 
+                 
+   
+                
+
+
         </View>
       
      
@@ -225,6 +321,9 @@ class Home extends PureComponent {
                     )}
                     keyExtractor= {(item)=> item.location_id.toString()}
                 />
+
+
+              
 
 
 
